bab3ac07823c6c1948a86628f76b4338b7c078ce
Le Duc Anh Tai <taile13092k5@gmail.com>
2024-03-02 20:36:08 +0700

    teo

---
+++ bai3ck.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin,s);
+	for(int i=0;i<s.size();i++)
+	{
+		if(i>=1&&s[i-1]=' '&&s[i]=' ')
+		{
+			
+		}
+	}
+    return 0;
+}
